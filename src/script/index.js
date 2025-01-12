0abef01de2518b91e8b516ae3d358d318817e4f8
ddededodediamante <lukitadiamante5@gmail.com>
2025-01-12 18:37:13 -0300

    Search bar

--- src/script/index.js
+++ src/script/index.js
@@ -1,104 +1,124 @@
 async function getExtCode(id) {
-    return await fetch(`/extensions/code/${id}.js`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            } else return response.text();
-        })
-        .then(data => {
-            return data;
-        })
-        .catch(error => {
-            console.error(`Error fetching extension ${id}:`, error);
-            return null;
-        });
+  return await fetch(`/extensions/code/${id}.js`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      } else return response.text();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error(`Error fetching extension ${id}:`, error);
+      return null;
+    });
 }
 
 async function downloadExt(id) {
-    const data = await getExtCode(id);
+  const data = await getExtCode(id);
 
-    const blob = new Blob([data], { type: 'text/javascript' });
-    const url = URL.createObjectURL(blob);
+  const blob = new Blob([data], { type: 'text/javascript' });
+  const url = URL.createObjectURL(blob);
 
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = `${id}.js`;
-    document.body.appendChild(a);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = `${id}.js`;
+  document.body.appendChild(a);
 
-    a.click();
-    URL.revokeObjectURL(url);
+  a.click();
+  URL.revokeObjectURL(url);
 
-    document.body.removeChild(a);
+  document.body.removeChild(a);
 }
 
 async function copyExt(id) {
-    const data = await getExtCode(id);
+  const data = await getExtCode(id);
 
-    await navigator.clipboard.writeText(data);
-}
-
-function openURL(url) {
-    window.location.href = url;
+  await navigator.clipboard.writeText(data);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('/extensions/extensions.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            } else return response.json();
-        })
-        .then(data => {
-            if (!data) throw new Error('Extensions data is empty');
+  let extensions = [];
 
-            const gallery = document.getElementById('gallery');
+  fetch('/extensions/extensions.json')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      } else return response.json();
+    })
+    .then((data) => {
+      if (!data) throw new Error('Extensions data is empty');
 
-            for (let i = 0; i < data.length; i++) {
-                const ext = data[i];
-                const newElement = document.createElement('div');
-                newElement.id = ext.id;
-                newElement.className = 'extension';
+      extensions = data;
+      renderExtensions(extensions);
+    })
+    .catch((error) => {
+      console.error('Error fetching extensions:', error);
+    });
 
-                let nameDiv = `<div id="name">
-                <p>${ext.name}</p>`
+  function renderExtensions(extList) {
+    const gallery = document.getElementById('gallery');
+    gallery.innerHTML = '';
 
-                if (ext.canBeUsedOn.tw === true) {
-                    nameDiv += `
-                    <button onclick="openURL('https://turbowarp.org')">
-                      <img src = "/src/static/icons/tw.svg" alt = "TurboWarp" />
-                    </button>`;
-                }
-                if (ext.canBeUsedOn.pm === true) {
-                    nameDiv += `
-                    <button onclick="openURL('https://penguinmod.com')">
-                      <img src="/src/static/icons/pm.svg" alt="PenguinMod" />
-                    </button>`;
-                }
-                if (ext.unfinished === true) {
-                    nameDiv += `
-                    <button disabled title="Work in progress">
-                      <img src="/src/static/icons/hammer.svg" />
-                    </a>`
-                }
+    if (extList?.length === 0) {
+      gallery.innerHTML = '<strong>No extensions found.</strong>';
+      return;
+    }
 
-                nameDiv += `
-                </div>
-                <p class="description">${ext.description}</p>`;
+    extList.forEach((ext) => {
+      const newElement = document.createElement('div');
+      newElement.id = ext.id;
+      newElement.className = 'extension';
 
-                newElement.innerHTML = `
-                <img src="/extensions/thumbnail/${ext.id}.${ext.imgFormat ?? 'svg'}" alt="${ext.name}" id="thumbnail" />
+      let nameDiv = `<div id="name">\n<p>${ext.name}</p>`;
 
-                ${nameDiv}
+      if (ext.canBeUsedOn.tw === true) {
+        nameDiv += `
+          <button onclick="window.open('https://turbowarp.org', '_blank')">
+          <img src = "/src/static/icons/tw.svg" alt = "TurboWarp" />
+          </button>`;
+      }
+      if (ext.canBeUsedOn.pm === true) {
+        nameDiv += `
+          <button onclick="window.open('https://penguinmod.com', '_blank')">
+          <img src="/src/static/icons/pm.svg" alt="PenguinMod" />
+          </button>`;
+      }
+      if (ext.unfinished === true) {
+        nameDiv += `
+          <button disabled title="Work in progress">
+          <img src="/src/static/icons/hammer.svg" />
+          </button>`;
+      }
 
-                <div id="buttons">
-                <button onclick="downloadExt('${ext.id}')">Download</button>
-                <button onclick="copyExt('${ext.id}')">Copy</button>
-                </div>`;
+      nameDiv += `</div>
+      <p class="description">${ext.description}</p>`;
 
-                gallery.appendChild(newElement);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching extensions:', error);
-        });
+      newElement.innerHTML = `<img src="/extensions/thumbnail/${ext.id}.${
+        ext.imgFormat ?? 'svg'
+      }" alt="${ext.name}" id="thumbnail" />
+
+      ${nameDiv}
+
+      <div id="buttons">
+      <button onclick="downloadExt('${ext.id}')">Download</button>
+      <button onclick="copyExt('${ext.id}')">Copy</button>
+      </div>`;
+
+      gallery.appendChild(newElement);
+    });
+  }
+
+  window.filterExtensions = function () {
+    const query = document
+      .getElementById('search-bar')
+      .value.toLowerCase()
+      .trim();
+    const filteredExtensions = extensions.filter(
+      (ext) =>
+        ext.name.toLowerCase().includes(query) ||
+        (ext.description && ext.description.toLowerCase().includes(query))
+    );
+    renderExtensions(filteredExtensions);
+  };
 });
