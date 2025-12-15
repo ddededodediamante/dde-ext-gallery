b0d6d3f7c4ba12594d0e09581c3f91b5cd2ecee7
ddededodediamante <lukitadiamante5@gmail.com>
2025-12-14 23:49:17 -0300

    wow

--- extensions/code/ahh.js
+++ extensions/code/ahh.js
@@ -9,11 +9,26 @@
     }
 
     toReporterContent() {
-      const container = document.createElement("iframe");
-      container.src = "https://classic.minecraft.net/";
-      container.width = 300;
-      container.style.width = 300;
-      return container;
+      const scale = 0.5;
+      const width = 300;
+      const height = 200;
+
+      const wrapper = document.createElement("div");
+      wrapper.style.width = width + "px";
+      wrapper.style.height = height + "px";
+      wrapper.style.overflow = "hidden";
+
+      const iframe = document.createElement("iframe");
+      iframe.src = "https://classic.minecraft.net/";
+
+      iframe.style.transform = `scale(${scale})`;
+      iframe.style.transformOrigin = "top left";
+
+      iframe.style.width = `${width / scale}px`;
+      iframe.style.height = `${height / scale}px`;
+
+      wrapper.appendChild(iframe);
+      return wrapper;
     }
 
     toMonitorContent() {
@@ -35,86 +50,62 @@
     }
   }
 
-  let setupShapeshifting = () => {};
-  if (Scratch.gui) {
-    Scratch.gui.getBlockly().then(SB => {
-      console.log(SB)
-      setupShapeshifting = () => {
-        SB.Blocks["ddeTestExtEvil_shapeshifting"] = {
-          init: function () {
-            this.appendDummyInput()
-              .appendField("i am")
-              .appendField(
-                new SB.FieldDropdown([
-                  ["a statement", "STATEMENT"],
-                  ["an output", "OUTPUT"],
-                ]),
-                "MODE"
-              );
-            this.setColour("#4290f5");
-            this.setTooltip("tooltip");
-            
-            this.mode_ = this.getFieldValue("MODE") || "STATEMENT";
-            this.updateShape();
-          },
+  class PenguinModType {
+    constructor(where, projectId) {
+      this.customId = "ddeTextExtEvil.penguinmod";
+      this.where = where || "homepage";
+      this.projectId = projectId || "skibidi";
+    }
 
-          updateShape: function () {
-            if (this.mode_ === "OUTPUT") {
-              this.setOutput(true, "Number");
-              this.setPreviousStatement(false);
-              this.setNextStatement(false);
-            } else if (this.mode_ === "STATEMENT") {
-              this.setOutput(false);
-              this.setPreviousStatement(true);
-              this.setNextStatement(true);
-            }
+    toString() {
+      return "Minecraft";
+    }
 
-            if (this.render) this.render();
-          },
+    toReporterContent() {
+      const url =
+        this.where === "homepage"
+          ? "https://penguinmod.com/"
+          : `https://studio.penguinmod.com/#${this.projectId}`;
 
-          mutationToDom: function () {
-            const container = document.createElement("mutation");
-            container.setAttribute("mode", this.mode_);
-            return container;
-          },
+      const scale = 0.5;
+      const width = 300;
+      const height = 200;
 
-          domToMutation: function (xml) {
-            const m = xml.getAttribute("mode");
-            this.mode_ = m;
-            this.updateShape();
-          },
+      const wrapper = document.createElement("div");
+      wrapper.style.width = width + "px";
+      wrapper.style.height = height + "px";
+      wrapper.style.overflow = "hidden";
 
-          saveExtraState: function () {
-            return {
-              mode: this.mode_,
-            };
-          },
+      const iframe = document.createElement("iframe");
+      iframe.src = url;
 
-          loadExtraState: function (state) {
-            console.log(state)
-            const m =
-              (state && state.mode) ||
-              this.getFieldValue("MODE") ||
-              "STATEMENT";
-            this.mode_ = m;
-            this.updateShape();
-          },
+      iframe.style.transform = `scale(${scale})`;
+      iframe.style.transformOrigin = "top left";
 
-          onchange: function (e) {
-            if (
-              e.type === "change" &&
-              e.name === "MODE" &&
-              e.element === "field" &&
-              e.blockId === this.id
-            ) {
-              this.mode_ = e.newValue;
-              this.unplug(true);
-              this.updateShape();
-            }
-          },
-        };
-      };
-    });
+      iframe.style.width = `${width / scale}px`;
+      iframe.style.height = `${height / scale}px`;
+
+      wrapper.appendChild(iframe);
+      return wrapper;
+    }
+
+    toMonitorContent() {
+      const container = document.createElement("p");
+      container.innerText = "yeah it doesn't work here";
+      return container;
+    }
+
+    toListItem() {
+      return this.toMonitorContent();
+    }
+
+    toListEditor() {
+      return this.toString();
+    }
+
+    fromListEditor(edit) {
+      return new PenguinModType(edit);
+    }
   }
 
   class ddeTestExtEvil {
@@ -127,12 +118,10 @@
     }
 
     getInfo() {
-      setupShapeshifting();
-
       return {
         id: "ddeTestExtEvil",
         name: "dde Test Extension (EVIL)",
-        color1: "#4290f5",
+        color1: "#4abcdf",
         blocks: [
           {
             opcode: "compiledIfNot",
@@ -144,39 +133,37 @@
             },
           },
           {
-            blockType: Scratch.BlockType.XML,
-            xml: `<block type="ddeTestExtEvil_shapeshifting"><mutation mode="STATEMENT"></mutation></block>`,
-          },
-          //(() => {
-          //  setupShapeshifting();
-          //  return {
-          //    opcode: "shapeshifting",
-          //    blockType: Scratch.BlockType.REPORTER,
-          //    text: "i am [MODE]",
-          //    arguments: {
-          //      MODE: {
-          //        type: Scratch.ArgumentType.STRING,
-          //        menu: "MODE",
-          //      },
-          //    },
-          //  };
-          //})(),
-          {
             opcode: "minecraft",
             blockType: Scratch.BlockType.REPORTER,
             blockShape: Scratch.BlockShape.TICKET,
             text: "minecraft",
+            disableMonitor: true
+          },
+          {
+            opcode: "penguinmodHomepage",
+            blockType: Scratch.BlockType.REPORTER,
+            blockShape: Scratch.BlockShape.TICKET,
+            text: "penguinmod homepage",
+            disableMonitor: true
+          },
+          {
+            opcode: "penguinmodProject",
+            blockType: Scratch.BlockType.REPORTER,
+            blockShape: Scratch.BlockShape.TICKET,
+            text: "penguinmod project with id [projectId]",
+            arguments: {
+              projectId: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "skibidi",
+              },
+            },
+            disableMonitor: true
           },
         ],
-        menus: {
-          MODE: ["STATEMENT", "OUTPUT"],
-        },
       };
     }
 
     getCompileInfo() {
-      setupShapeshifting();
-
       return {
         ir: {
           compiledIfNot: (generator, block) => ({
@@ -185,16 +172,6 @@
             whenTrue: generator.descendSubstack(block, "SUBSTACK"),
             whenFalse: [],
           }),
-          shapeshifting: (generator, block) => {
-            console.log(generator, block);
-            const value = block.fields.MODE.value;
-            return {
-              kind: value === "OUTPUT" ? "expression" : "stack",
-              args: {
-                MODE: { kind: "constant", value: value },
-              },
-            };
-          },
         },
         js: {
           compiledIfNot: (node, compiler, imports) => {
@@ -208,16 +185,6 @@
             }
             compiler.source += `}\n`;
           },
-          shapeshifting: (node, compiler) => {
-            const rawMode = node.args.MODE.value;
-
-            if (rawMode === "OUTPUT") {
-              compiler.source += `return (() => Math.random())()`;
-              return `return (() => Math.random())();`;
-            } else {
-              compiler.source += `console.log("I am a shapeshifting statement.");\n`;
-            }
-          },
         },
       };
     }
@@ -228,15 +195,17 @@
         util.startBranch(1, false);
       }
     }
-    shapeshifting(args, util) {
-      if (args.MODE === "OUTPUT") {
-        return Math.random();
-      } else {
-        console.log("I am a shapeshifting statement (runtime).");
-      }
-    }
+
     minecraft() {
       return new MinecraftType();
+    }
+
+    penguinmodHomepage() {
+      return new PenguinModType("homepage");
+    }
+
+    penguinmodProject({ projectId }) {
+      return new PenguinModType("project", projectId || "skibidi");
     }
   }
 
