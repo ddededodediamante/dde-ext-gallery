296c54a0f242a8fe41efa91582ceff47688cc256
ddededodediamante <lukitadiamante5@gmail.com>
2026-05-20 01:20:28 -0300

    the Smallest little optimizations

---
+++ extensions/code/_template.js
@@ -0,0 +1,22 @@
+(function (Scratch) {
+  const isPM = Scratch.extensions?.isPenguinMod ?? false;
+
+  if (!Scratch.extensions.unsandboxed) {
+    window.alert('The extension "Template" must be ran unsandboxed!');
+    throw new Error('The extension "Template" must be ran unsandboxed!');
+  }
+
+  class ddeTemplate {
+    getInfo() {
+      return {
+        id: "ddeTemplate",
+        name: "Template",
+        color1: "#59c081",
+        blocks: [],
+        menus: {},
+      };
+    }
+  }
+
+  Scratch.extensions.register(new ddeTemplate());
+})(Scratch);
